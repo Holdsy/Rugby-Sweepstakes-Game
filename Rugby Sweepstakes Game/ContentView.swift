b06f34c947965120c9fd1780dec2e8dbf1a135ec
Mark Holdsworth <mholdsworth@me.com>
2025-12-03 10:32:27 +0000

    Initial Commit

---
+++ Rugby Sweepstakes Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rugby Sweepstakes Game
+//
+//  Created by Mark Holdsworth on 03/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rugby Sweepstakes Game/Rugby_Sweepstakes_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rugby_Sweepstakes_GameApp.swift
+//  Rugby Sweepstakes Game
+//
+//  Created by Mark Holdsworth on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Rugby_Sweepstakes_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
